07070792b89efd967c24857a4c5f5db352e5105e
Al <devbyal@users.noreply.github.com>
2015-04-18 19:19:27 -0700

    Added a test class

---
+++ Git TestTests/DBATest.h
@@ -0,0 +1,13 @@
+//
+//  DBATest.h
+//  Git Test
+//
+//  Created by Al on 4/18/15.
+//  Copyright (c) 2015 Dev By Al. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBATest : NSObject
+
+@end
---
+++ Git TestTests/DBATest.m
@@ -0,0 +1,13 @@
+//
+//  DBATest.m
+//  Git Test
+//
+//  Created by Al on 4/18/15.
+//  Copyright (c) 2015 Dev By Al. All rights reserved.
+//
+
+#import "DBATest.h"
+
+@implementation DBATest
+
+@end
